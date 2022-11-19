096c4d6b2bb5f252cde182dc8bfa65477b8ba17f
Dmitry Mishin <dmitrymishin@Dmitrys-MacBook-Pro.local>
2022-11-19 01:35:41 +0100

    restruct rxjs

---
+++ src/shared/rxjs/hooks/index.ts
@@ -0,0 +1,5 @@
+export { useCombinedObservables } from './useCombinedObservables';
+export { useEventHandler } from './useEventHandler';
+export { useMergedObservables } from './useMergedObservables';
+export { useObservable } from './useObservable';
+export { useUnsubscribe } from './useUnsubscribe';
